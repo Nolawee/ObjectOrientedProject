b34a98f351b641aa00d0f3a95ed400ccac586963
Shawn <sapols@yahoo.com>
2017-04-03 19:56:43 -0600

    Make the constants of Card static.

--- src/main/java/blackjack/model/Card.java
+++ src/main/java/blackjack/model/Card.java
@@ -10,21 +10,21 @@ public class Card {
 	final int KING = 13;
 	final int ACE = 1;
 	
-	final String DIAMONDS = "d";
-	final String CLUBS = "c";
-	final String HEARTS = "h";
-	final String SPADES = "s";
+	final static String DIAMONDS = "d";
+	final static String CLUBS = "c";
+	final static String HEARTS = "h";
+	final static String SPADES = "s";
 	
-	final char BLACK = 'b';
-	final char RED = 'r';
+	final static char BLACK = 'b';
+	final static char RED = 'r';
 			
 	
 	public Card(int num, String suit) throws Exception {
 		if (isProperNumber(num)) number = num;
-		else throw new Exception("Card number must be between 1 and 13 (Between ACE-KING)");
+		else throw new Exception("Invalid number: " + num + ". Card number must be between 1 and 13 (Between ACE-KING)");
 		
 		if (isProperSuit(suit)) this.suit = suit;
-		else throw new Exception("Card suit must be either 'DIAMONDS', 'CLUBS', 'HEARTS', or 'SPADES'.");
+		else throw new Exception("Invalid suit: " + suit + ". Card suit must be either 'DIAMONDS', 'CLUBS', 'HEARTS', or 'SPADES'.");
 	}
 	
 	public void reveal() {
@@ -53,7 +53,7 @@ public class Card {
 	}
 	
 
-	//----Helper methods---------------------------------------------------------------------------------
+	//----Helper methods-----------------------------------------------------------------------------------------------------------
 	
 	public boolean isProperNumber(int num) {
 		if (num >= 1 && num <= 13) return true;
