f3a183caebec52e39f71625b6ff397500c9de788
Shawn <sapols@yahoo.com>
2017-04-03 20:55:20 -0600

    Starting implementation of Player class, per our class diagram.

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -1,5 +1,12 @@
 package blackjack.model;
 
+//Subclasses: HumanPlayer, ComputerPlayer, Dealer
 public abstract class Player {
-	//Subclasses: HumanPlayer, ComputerPlayer, Dealer
+    private String name;
+    private int gamesWon;
+    private boolean isPlaying;
+    private Card[] hand;
+    private Card[] splitHand; //only used after choosing to "split"
+    
+    //abstract public void takeTurn(); //(Commented out to avoid errors in unimplemented subclasses)
 }
