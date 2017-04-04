de77ef7087ee4508ca40ac4e6d320bc7cbfd838c
Shawn <sapols@yahoo.com>
2017-04-03 18:31:36 -0600

    Implement Card class.

--- src/main/java/blackjack/model/Card.java
+++ src/main/java/blackjack/model/Card.java
@@ -1,5 +1,69 @@
 package blackjack.model;
 
 public class Card {
-	//Implementation of Card here
+	private int number;
+	private String suit;
+	private boolean isFaceUp = false;
+	
+	final int JACK = 11;
+	final int QUEEN = 12;
+	final int KING = 13;
+	final int ACE = 14;
+	
+	final String DIAMONDS = "d";
+	final String CLUBS = "c";
+	final String HEARTS = "h";
+	final String SPADES = "s";
+	
+	final char BLACK = 'b';
+	final char RED = 'r';
+			
+	
+	public Card(int num, String suit) throws Exception {
+		if (isProperNumber(num)) number = num;
+		else throw new Exception("Card number must be between 1 and 14 (Between 1-ACE)");
+		
+		if (isProperSuit(suit)) this.suit = suit;
+		else throw new Exception("Card suit must be either 'DIAMONDS', 'CLUBS', 'HEARTS', or 'SPADES'.");
+	}
+	
+	public void reveal() {
+		isFaceUp = true;
+	}
+	
+	public boolean isFaceUp() {
+		return isFaceUp;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public String getSuit() {
+		return suit;
+	}
+	
+	public char getColor() {
+		switch (suit) {
+			case DIAMONDS: return RED;
+			case CLUBS: return BLACK; 
+			case HEARTS: return RED;
+			default: return BLACK; //SPADES (Java complains without a default case).
+		}
+	}
+	
+
+	//----Helper methods---------------------------------------------------------------------------------
+	
+	public boolean isProperNumber(int num) {
+		if (num >= 1 && num <= 14) return true;
+		else return false;
+	}
+	
+	public boolean isProperSuit(String suit) {
+		if (suit.equals(DIAMONDS) || suit.equals(CLUBS) || suit.equals(HEARTS) || suit.equals(SPADES))
+			return true;
+		else
+			return false;
+	}
 }
