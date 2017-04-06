088271e5b0cf57a016527b0bb0bae278c3625085
Shawn <sapols@yahoo.com>
2017-04-05 21:56:38 -0600

    Second implementation of Chip class that can "divyUpChips".

--- src/main/java/blackjack/model/Chip.java
+++ src/main/java/blackjack/model/Chip.java
@@ -1,7 +1,7 @@
 package blackjack.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Chip {
 	private int value = 0;
@@ -14,29 +14,29 @@ public class Chip {
 		return value;
 	}
 	
-	public static Chip[] divyUpChips(int amount) { //THIS SHOULD RETURN A MAP, NOT AN ARRAY
-	    List<Chip> chips = new ArrayList<Chip>();
-	    int[] chipValues = new int[]{50000, 20000, 5000, 500, 100, 50, 25, 10, 5, 1};
-	    
-	    for (int val: chipValues) {
-	        int i;
-	        for (i = 0; i < amount/val; i++) {
-	            chips.add(new Chip(val));
-	        }
-	        amount = amount - (i*val);
-	    }
-	    
-	    
-	    return chips.toArray(new Chip[chips.size()]);
-	}
+	/*
+	 * TODO: Consider that this representation of chips 
+	 *       makes the member variable "value" obsolete. 
+	 */
+	public static Map<Integer, Integer> divyUpChips(int amount) { 
+        Map<Integer, Integer> chips = new LinkedHashMap<Integer, Integer>();
+        int[] chipValues = new int[]{50000, 20000, 5000, 500, 100, 50, 25, 10, 5, 1};
+        
+        for (int val: chipValues) {
+            int numChips = amount/val;
+            chips.put(val, numChips);
+            amount = amount - numChips*val;
+        }
+        
+        chips.values().removeIf(v -> v==0);
+        return chips;
+    }
+	
+	//----Main method for testing------------------------------------------------------
 	
 //	public static void main(String[] args) {
-//	    Chip[] chips = divyUpChips(1100732);
+//	    Map chips = divyUpChips(1100732);
 //	    
-//	    for (Chip chip: chips) {
-//	        System.out.println(chip.getValue());
-//	    }
-//	    
-//	    
+//	    System.out.println(chips);
 //	}
 }
