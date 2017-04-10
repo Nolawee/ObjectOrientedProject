165c22cb534fd9359e27ccb86b147ace1ee5b253
Shawn <sapols@yahoo.com>
2017-04-10 12:58:06 -0600

    Table implements Serializable now.

--- src/main/java/blackjack/model/Table.java
+++ src/main/java/blackjack/model/Table.java
@@ -1,5 +1,8 @@
 package blackjack.model;
 
-public class Table {
+import java.io.Serializable;
 
+public class Table implements Serializable {
+    //Implement Serializable for writing/loading 
+    //(Unless we learn a better way to do this during lecture)
 }
