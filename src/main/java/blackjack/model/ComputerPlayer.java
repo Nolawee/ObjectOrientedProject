e6367304e8740666f42667729958b8d928e87d96
Shawn <sapols@yahoo.com>
2017-04-11 13:07:42 -0600

    Add basic member variables to Human and Computer Players.

--- src/main/java/blackjack/model/ComputerPlayer.java
+++ src/main/java/blackjack/model/ComputerPlayer.java
@@ -1,5 +1,9 @@
 package blackjack.model;
 
 public class ComputerPlayer extends Player {
-	//Implementation of ComputerPlayer here
+    int currentTotal; //Sum of chip values
+    Chips currentBet; //We'll be representing bets with chips themselves, not an int.
+    Chips chips;
+    boolean mustHit;
+    //Implementation of ComputerPlayer here
 }
--- src/main/java/blackjack/model/HumanPlayer.java
+++ src/main/java/blackjack/model/HumanPlayer.java
@@ -1,5 +1,8 @@
 package blackjack.model;
 
 public class HumanPlayer extends Player {
-	//Implementation of HumanPlayer here
+    int currentTotal; //Sum of chip values
+    Chips currentBet; //We'll be representing bets with chips themselves, not an int.
+    Chips chips;
+    //Implementation of HumanPlayer here
 }
