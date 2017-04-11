5b9c3d1ad80c52153bfc0ac236aee4331ee36657
Shawn <sapols@yahoo.com>
2017-04-10 20:57:05 -0600

    Correct comment.

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -8,7 +8,6 @@ public abstract class Player {
     private Card[] splitHand; //only used after choosing to "split"
     
     /*abstract public void takeTurn();*/ //NOT USED because of TurnManager. 
-                                       //Leaving this here temporarily to alert our team
-                                       //because this varies from our class diagram.
-                                     
+                                         //Leaving this here temporarily to alert our team
+                                         //because this varies from our class diagram.              
 }
