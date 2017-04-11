43c90a6ebb4e197fb3b2da6da6b60b492a397de1
Shawn <sapols@yahoo.com>
2017-04-10 20:55:18 -0600

    Remove takeTurn() from Player class because TurnManager does that now.

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -4,12 +4,11 @@ package blackjack.model;
 public abstract class Player {
     private String name;
     private int gamesWon;
-    private boolean isPlaying; //PROBABLY NOT NEEDED
     private Card[] hand;
     private Card[] splitHand; //only used after choosing to "split"
     
-    //abstract public void takeTurn(); //(PROBABLY NOT NEEDED because of TurnManager. 
-                                       //Commented out to avoid errors in unimplemented subclasses)
-    
-    
+    /*abstract public void takeTurn();*/ //NOT USED because of TurnManager. 
+                                       //Leaving this here temporarily to alert our team
+                                       //because this varies from our class diagram.
+                                     
 }
