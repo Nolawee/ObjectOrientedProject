33a027054801351b5132f1660c7db6ec1d118c47
Shawn <sapols@yahoo.com>
2017-04-18 17:45:01 -0600

    Structure implementation of PlayHandPhase (considerations in comments).

--- src/main/java/blackjack/model/PlayHandPhase.java
+++ src/main/java/blackjack/model/PlayHandPhase.java
@@ -5,10 +5,25 @@ public class PlayHandPhase implements State {
     public void takeTurn(TurnManager wrapper, Player player, Dealer dealer) {
         wrapper.setState(new DealerHandPhase());
         //TODO: Add code for the play hand phase of this turn
+        //NOTE: We need to figure out how we're going to prompt human players
+        //      for input here. 
+        //
+        //      If we're making a fancy GUI, those prompts should probably
+        //      happen elsewhere. 
+        //      If we're just gonna do a command-line interface, right here
+        //      would be the place to do that. 
+        //
+        //      The code below assumes we're using just a command-line interface.
+        
+        while (player.isPlaying && !player.hasBusted()) {
+            //TODO: code the exchange between player and dealer here.
+            //      This phase of the turn ends when the player chooses 
+            //      "stand" or hits too many times and goes bust. 
+        }
    
     }
     
     //----Helper methods------------------------------------------------------
-    //TODO: Add code
+    //TODO: Add code if needed
     
 }
