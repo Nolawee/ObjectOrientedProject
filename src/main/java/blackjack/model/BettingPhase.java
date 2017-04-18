b18dc686ff60402095df67cb41dc9e8b0bb0c032
Shawn <sapols@yahoo.com>
2017-04-18 17:44:02 -0600

    Fully implement BettingPhase state (for a simple command-line UI).

--- src/main/java/blackjack/model/BettingPhase.java
+++ src/main/java/blackjack/model/BettingPhase.java
@@ -1,14 +1,29 @@
 package blackjack.model;
 
+import java.util.Scanner;
+
 public class BettingPhase implements State {
     @Override
     public void takeTurn(TurnManager wrapper, Player player, Dealer dealer) {
         wrapper.setState(new PlayHandPhase()); 
-        //TODO: Add code for the betting phase of this turn
-   
+        player.isPlaying();
+        //TODO: Add code for the betting phase of this turn.
+        //NOTE: We need to figure out how we're going to prompt human players
+        //      for input here. 
+        //
+        //      If we're making a fancy GUI, those prompts should probably
+        //      happen elsewhere. 
+        //      If we're just gonna do a command-line interface, right here
+        //      would be the place to do that. 
+        //
+        //      The code below assumes we're using just a command-line interface.
+        
+        int bet;
+        System.out.println("How much would you like to bet on this hand?");
+        Scanner scan = new Scanner(System.in);
+        bet = scan.nextInt();
+        scan.close();
+        
+        player.bet(bet);
     }
-    
-    //----Helper methods------------------------------------------------------
-    //TODO: Add code
-    
 }
