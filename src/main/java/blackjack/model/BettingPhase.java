7aeda207c838f49586a28fdc70437cafa11c847f
Shawn <sapols@yahoo.com>
2017-04-18 20:43:36 -0600

    Add a loop to allow multiple chips to be bet in BettingPhase.

--- src/main/java/blackjack/model/BettingPhase.java
+++ src/main/java/blackjack/model/BettingPhase.java
@@ -7,7 +7,6 @@ public class BettingPhase implements State {
     public void takeTurn(TurnManager wrapper, Player player, Dealer dealer) {
         wrapper.setState(new PlayHandPhase()); 
         player.startPlaying();
-        //TODO: Add code for the betting phase of this turn.
         //NOTE: We need to figure out how we're going to prompt human players
         //      for input here. 
         //
@@ -19,13 +18,28 @@ public class BettingPhase implements State {
         //      The code below assumes we're using just a command-line interface.
         
         int bet;
-        System.out.println("How much would you like to bet on this hand?");
+        System.out.println("How much would you like to bet on this hand? When done entering chips to bet, enter -1.");
         //This next printout won't be needed when the view is always showing the chips
-        System.out.println("The chips you have to choose from are: "); player.getChips().printChips();
-        Scanner scan = new Scanner(System.in);
-        bet = scan.nextInt();
-        scan.close();
+        System.out.println("The chips you have to choose from are: " ); player.getChips().printChips(); 
+        System.out.println();
         
-        player.bet(bet);
+        Scanner scan = new Scanner(System.in);
+        int playerInput = 0;
+        while (true) {
+            try {
+                System.out.println("Bet: ");
+                playerInput = scan.nextInt();
+                if (playerInput == -1) { //Someone might think of something cooler than typing -1. I'm not creative.
+                    break;
+                } else {
+                    bet = playerInput;
+                    player.bet(bet);
+                }    
+            } catch (Exception e) {
+                System.out.println("You don't have a chip of value: " + playerInput + ". Try again: ");    
+            }
+        }  
+        scan.close();
     }
+    
 }
