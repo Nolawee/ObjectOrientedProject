108bddd819806d7d7b12d32140e77f36e7273c29
Shawn <sapols@yahoo.com>
2017-04-24 16:54:55 -0600

    Fully implement PlayHandPhase (assumes command-line interface).

--- src/main/java/blackjack/model/PlayHandPhase.java
+++ src/main/java/blackjack/model/PlayHandPhase.java
@@ -1,10 +1,11 @@
 package blackjack.model;
 
+import java.util.Scanner;
+
 public class PlayHandPhase implements State {
     @Override
     public void takeTurn(TurnManager wrapper, Player player, Dealer dealer) {
         wrapper.setState(new DealerHandPhase());
-        //TODO: Add code for the play hand phase of this turn
         //NOTE: We need to figure out how we're going to prompt human players
         //      for input here. 
         //
@@ -13,57 +14,83 @@ public class PlayHandPhase implements State {
         //      If we're just gonna do a command-line interface, right here
         //      would be the place to do that. 
         //
-        //      The code below assumes we're using just a command-line interface.
+        //      The code below assumes we're using just a command-line interface
+        //      and fully implements everything this class would need to do 
+        //      (but could be improved, especially the user prompts).
         
         dealer.shuffleDeck();
         dealer.dealStartingHands(player);
-        numHits = 2; //tracks number of times player has chosen to hit to add to card array
-        Card card1 = player.hand(0);
-        Card card2 = player.hand(1);
-        if(card1.getNumber() == card2.getNumber()){
-            //check for possibility of splitting hand
-            System.out.println("Would you like to split your hand? (yes/no)");
-            String yn = scanner.next();
-            if(yn == "yes"){
-                //I have no idea if this is how you switch to the new state
-                //so im just guessing, but this is where we go from normal hand to split
-                wrapper.setState(new SplitHandPhase());
-            }
-        }
-
-        while (player.isPlaying() && !player.hasBusted()) {
-            //TODO: code the exchange between player and dealer here.
-            //      This phase of the turn ends when the player chooses 
-            //      "stand" or hits too many times and goes bust. 
-
-            total = player.getHandTotal();
-            System.out.println("You are currently at" + total);
-            System.out.println("Would you like to hit or stand?");
-            String hs = scanner.next();
-            if(hs == "hit"){
-                //I was not sure why we would need an extra step to 
-                //approve getting a card, or how I would go about coding that
-                //so I just did player.request
-                Card sHand1 = Player.requestDeal();
-                HumanPlayer.setHand(numHits,hand1);
-            }
-            else if(hs == "stand"){
-                wrapper.nextPlayer(); //end turn and move to next player
-            }
-            else{//error check
-                System.out.println("Please type hit or stand");
-            }
-            if(total >21){ //check for bust to display message
-                System.out.println("You Busted with a total of" + total); 
-                player.setPlaying(false);
-                player.won(false);
-            }
-            numHits++;
-        }
-   
+        checkForSplitHand(wrapper, player, dealer);
+        
+        playHand(player, dealer);
     }
     
     //----Helper methods------------------------------------------------------
-    //TODO: Add code if needed
     
+    /*
+     * Checks the two cards of a player's starting hand and 
+     * asks them if they want to split their hand when the 
+     * values of the cards match. 
+     * Split the hand if they respond yes. 
+     */
+    public void checkForSplitHand(TurnManager wrapper, Player player, Dealer dealer) {
+        if (player instanceof HumanPlayer) { //Necessary because we don't have a way to handle computer splitting yet
+            Card card1 = player.getHand().get(0);
+            Card card2 = player.getHand().get(1);
+            if (card1.getNumber() == card2.getNumber()) {
+                
+                System.out.println("Would you like to split your hand? (yes/no)");
+                Scanner scan = new Scanner(System.in);
+                String yn = scan.next();
+                
+                //TODO: Error checking (catch bad responses and re-prompt)
+                if (yn.equalsIgnoreCase("yes") || yn.equalsIgnoreCase("y")) {
+                    player.split(dealer); //Splits the original hand into two hands
+                    wrapper.setState(new SplitHandPhase()); //Enter this phase before the DealerHandPhase
+                }
+                scan.close();
+            }
+        }
+    }
+    
+    /*
+     * Handles the exchange between a player and a dealer
+     * during a player's turn. Prompts the player on how to 
+     * react -- options are to hit, double, or stand. 
+     * Turn phase ends when a player stands or goes bust.
+     */
+    public void playHand(Player player, Dealer dealer) {
+        int handTotal;
+        Scanner scan = new Scanner(System.in);
+        
+        while (player.isPlaying() && !player.hasBusted()) {
+            handTotal = player.getHandTotal();
+            
+            player.printHand(); //Won't be needed when view is always showing hands
+            System.out.println("You are currently at " + handTotal + ".");
+            System.out.println("Would you like to hit or stand?");
+            String hds = scan.next();
+            
+            if (hds.equalsIgnoreCase("hit")) {
+                player.hit(dealer);
+            }
+            else if (hds.equalsIgnoreCase("double")) {
+                player.doubleBet(dealer);
+            }
+            else if (hds.equalsIgnoreCase("stand")) {
+                player.stand();
+            }
+            else { //Error checking (catch bad responses and re-prompt)
+                System.out.println("'" + hds + "' was not a valid response. Please type 'hit,' 'double,' or 'stand.'");
+            }
+            
+            if (player.hasBusted()) { 
+                System.out.println("You Busted with a total of " + handTotal); 
+                player.setIsPlaying(false);
+            }
+        }
+        scan.close();
+    }
+   
+ 
 }
