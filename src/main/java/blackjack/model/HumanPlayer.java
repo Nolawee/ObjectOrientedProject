cb4b5b458838ae4e35a98f9dee738b1de5cddcad
Shawn <sapols@yahoo.com>
2017-04-24 20:56:06 -0600

    Remove redundant member variables that are inherited from Player.

--- src/main/java/blackjack/model/HumanPlayer.java
+++ src/main/java/blackjack/model/HumanPlayer.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class HumanPlayer extends Player {
     private ArrayList<Card> splitHand; //only used after choosing to "split"
-    Chips currentBet; //We'll be representing bets with chips themselves, not an int.
-    Chips chips;
     
     public HumanPlayer(String name, int startingMoney) {
         setName(name);
