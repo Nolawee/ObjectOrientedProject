170b9ab6c562f306645eda80a251243ae595ab62
Nolawee Mengist <nome9478@colorado.edu>
2017-05-01 13:45:16 -0600

    save table state

--- src/main/java/blackjack/controller/Memento.java
+++ src/main/java/blackjack/controller/Memento.java
@@ -1,5 +1,9 @@
 package blackjack.controller;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import blackjack.model.Table;
 
 public class Memento { 
@@ -8,7 +12,14 @@ public class Memento {
 	
 	//save state
 	public Memento(Table state){ 
-		Table = state; 
+		Table = state;
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory .openSession();
+		session.beginTransaction(); 
+		session.save(Table);
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
 	}
 	//get state
 	public Table getState(){
