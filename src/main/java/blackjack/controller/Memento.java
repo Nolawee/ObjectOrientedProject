2db680f4554940b169d2ea6945a5c51c56e1dd50
Nolawee Mengist <nome9478@colorado.edu>
2017-05-01 22:10:43 -0600

    get most recent state

--- src/main/java/blackjack/controller/Memento.java
+++ src/main/java/blackjack/controller/Memento.java
@@ -9,20 +9,23 @@ import blackjack.model.Table;
 public class Memento { 
 	//Memento
 	private Table Table;
+	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	Session session = sessionFactory .openSession();
 	
 	//save state
 	public Memento(Table state){ 
 		Table = state;
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = sessionFactory .openSession();
 		session.beginTransaction(); 
 		session.save(Table);
 		session.getTransaction().commit();
 		session.close();
 		sessionFactory.close();
 	}
-	//get state
+	//gets most recent state
 	public Table getState(){
+		session.beginTransaction(); 
+		Table = sessionFactory.getCurrentSession().get(Table.class, 1); 
+		session.close();
 		return Table;
 	}
 	
